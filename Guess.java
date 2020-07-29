4ca621f3991aac0999f5d00de9599c3ce553a1c1
Nick Lopez <nbl42@nau.edu>
2020-07-29 11:56:03 -0700

    ready to submit

--- Guess.java
+++ Guess.java
@@ -1,11 +1,13 @@
 package Number_Guess;
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class Guess {
 
 	private static int guessCount;
 	private static Random randSeed;
+	private static Scanner sc;
 
 	public enum GuessResult {
 		TOO_HIGH, TOO_LOW, CORRECT
@@ -23,7 +25,7 @@ public class Guess {
 	}
 	
 	private static void incrementGuess() {
-		guessCount = (guessCount <= 0 ) ? 0 : guessCount + 1;
+		guessCount = (guessCount < 0 ) ? 0 : guessCount + 1;
 	}
 	
 	private static void resetGuesses() {
@@ -40,10 +42,29 @@ public class Guess {
 	 * @return enum for too high, too low, or correct responses.
 	 */
 	private static GuessResult checkGuess(int guess) {
-	  //TODO: Complete
-		incrementGuess();
-		System.out.printf("Current Guess: %d\n", guess);
-		return GuessResult.CORRECT;
+		if (sc == null) {
+			sc = new Scanner(System.in);
+		}
+		
+		System.out.printf("Is your number %d?\nType \'h\' if the number is too high, \'l\' if the number is too low, or \'c\'c if the number is correct.\n", guess);
+		String input = sc.next();
+		switch (input) {
+			case "h":
+			case "H":
+				incrementGuess();
+				return GuessResult.TOO_HIGH;
+			case "l":
+			case "L":
+				incrementGuess();
+				return GuessResult.TOO_LOW;
+			case "c":
+			case "C":
+				incrementGuess();
+				return GuessResult.CORRECT;
+			default:
+				System.out.println("Invalid input.");
+				return checkGuess(guess);
+		}
 	}
 	
 	public static void randomGuess() {
@@ -51,19 +72,22 @@ public class Guess {
 		int lowestNumber = 0;
 		int highestNumber = 100;
 		int currentGuess = randomNumber();
+		System.out.println("Please choose a number between 1 and 100.");
 		GuessResult lastResult =  checkGuess(currentGuess);  // guess random number between 1 and 100 to start.
 		while (lastResult != GuessResult.CORRECT ) {
 			switch (lastResult) {
 			case TOO_HIGH:
-				highestNumber = currentGuess + 1;
+				highestNumber = currentGuess - 1;
 				break;
 			case TOO_LOW:
 				lowestNumber = currentGuess + 1;
 				break;
+			default:
 			}
 			currentGuess = randomNumber(lowestNumber, highestNumber);
 			lastResult = checkGuess(currentGuess);
 		}
+		System.out.printf("The computer took %d guesses to find your number.", getGuessCount() );
 	}
 	
 	public static void binaryGuess() {
@@ -71,19 +95,22 @@ public class Guess {
 		int lowestNumber = 0;
 		int highestNumber = 100;
 		int currentGuess = 50;
+		System.out.println("Please choose a number between 1 and 100.");
 		GuessResult lastResult =  checkGuess(currentGuess);  // guess 50 to start
 		while (lastResult != GuessResult.CORRECT ) {
 			switch (lastResult) {
 			case TOO_HIGH:
-				highestNumber = currentGuess + 1;
+				highestNumber = currentGuess - 1;
 				break;
 			case TOO_LOW:
 				lowestNumber = currentGuess + 1;
 				break;
+			default:
 			}
-			currentGuess = (highestNumber - lowestNumber) / 2 + highestNumber;
+			currentGuess = (highestNumber - lowestNumber) / 2 + lowestNumber;
 			lastResult = checkGuess(currentGuess);
 		}
+		System.out.printf("The computer took %d guesses to find your number.", getGuessCount() );
 	}
 	
 	public static void userGuess() {
@@ -98,12 +125,19 @@ public class Guess {
 			}
 			userGuess = getUserGuess();
 		}
-		System.out.printf("Correct. The hidden number was %d.", hiddenNumber);	
+		System.out.printf("Correct. The hidden number was %d.\n", hiddenNumber);
+		System.out.printf("You took %d guesses to figure out the hidden number.\n", getGuessCount() );
 	}
 	
 	private static int getUserGuess() {
-		//TODO : finish 
-		return 1;
+		if (sc == null) {
+			sc = new Scanner(System.in);
+		}
+		int userNum;
+		incrementGuess();
+		System.out.println("Please guess a number:");
+		userNum = sc.nextInt();
+		return userNum;
 	}
 	
 }
--- GuessDriver.java
+++ GuessDriver.java
@@ -1,12 +1,38 @@
 package Number_Guess;
 
+import java.util.Scanner;
+
 public class GuessDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Number Guessing Program");
 		System.out.println("=======================");
-		Guess.randomGuess();
+		Scanner sc = new Scanner(System.in);
+		String input = "4";
+		do {
+			System.out.printf("Choose a program:\n1. Guess what the computer is thinking.\n2. Computer guesses what you are thinking. (easy)\n"
+					+ "3. Computer guesses what you are thinking. (hard)\n4. Exit\n");
+			input = sc.next();
+			switch(input) {
+				case "1":
+					Guess.userGuess();
+					break;
+				case "2":
+					Guess.randomGuess();
+					break;
+				case "3":
+					Guess.binaryGuess();
+					break;
+				case "4":
+					System.out.println("Thank You");
+					break;
+				default:
+					System.out.println("Please imput a number between 1 and 4.");
+					break;
+			}
+			
+		} while( !input.equals("4") );
+
 	}
 
 }
