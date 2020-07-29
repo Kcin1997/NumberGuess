95398c2337d115a502396f6d61d3f9edb2015860
Nick Lopez <nbl42@nau.edu>
2020-07-28 23:42:38 -0700

    Inital Commit

---
+++ Guess.java
@@ -0,0 +1,69 @@
+package Number_Guess;
+import java.util.Random;
+
+
+public class Guess {
+
+	private static int guessCount;
+	private static Random randSeed;
+
+	public enum GuessResult {
+		TOO_HIGH, TOO_LOW, CORRECT
+	}
+	
+	private static int randomNumber(int minNumber, int maxNumber) {
+		if (randSeed == null) {
+			randSeed = new Random();
+		}
+		return randSeed.nextInt(maxNumber - minNumber) + minNumber;
+	}
+	
+	private static int randomNumber() {
+		return randomNumber(1,101);
+	}
+	
+	private static void incrementGuess() {
+		guessCount = (guessCount <= 0 ) ? 0 : guessCount + 1;
+	}
+	
+	private static void resetGuesses() {
+		guessCount = 0;
+	}
+	
+	public static int getGuessCount() {
+		return guessCount;
+	}
+	
+	/** guessResult : sends a guess to the user and returns if it is too high, low or correct
+	 * 
+	 * @param guess guessed number
+	 * @return enum for too high, too low, or correct responses.
+	 */
+	private static GuessResult checkGuess(int guess) {
+	  //TODO: Complete
+		incrementGuess();
+		System.out.printf("Current Guess: %d\n", guess);
+		return GuessResult.CORRECT;
+	}
+	
+	public static void randomGuess() {
+		resetGuesses();
+		int lowestNumber = 0;
+		int highestNumber = 100;
+		int currentGuess = randomNumber();
+		GuessResult lastResult =  checkGuess(currentGuess);  // guess random number between 1 and 100 to start.
+		while (lastResult != GuessResult.CORRECT ) {
+			switch (lastResult) {
+			case TOO_HIGH:
+				highestNumber = currentGuess + 1;
+				break;
+			case TOO_LOW:
+				lowestNumber = currentGuess + 1;
+				break;
+			}
+			currentGuess = randomNumber(lowestNumber, highestNumber);
+			lastResult = checkGuess(currentGuess);
+		}
+	}
+	
+}
---
+++ GuessDriver.java
@@ -0,0 +1,12 @@
+package Number_Guess;
+
+public class GuessDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Number Guessing Program");
+		System.out.println("=======================");
+		Guess.randomGuess();
+	}
+
+}
